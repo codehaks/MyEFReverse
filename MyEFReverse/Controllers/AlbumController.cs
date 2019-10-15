fa14982b042415014f7dbca102c449fae5bc2bbd
Codehaks <ghods.hakim@gmail.com>
2019-10-15 09:05:51 +0330

    Add project files.

---
+++ MyEFReverse/Controllers/AlbumController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyEFReverse.Data;
+
+namespace MyEFReverse.Controllers
+{
+    public class AlbumController : Controller
+    {
+        private readonly ChinookContext _db;
+
+        public AlbumController(ChinookContext db)
+        {
+            _db = db;
+        }
+
+        [Route("api/albums")]
+        public IActionResult Index()
+        {
+            return Ok(_db.Albums);
+        }
+    }
+}
---
+++ MyEFReverse/Models/MediaTypes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyEFReverse.Models
+{
+    public partial class MediaTypes
+    {
+        public MediaTypes()
+        {
+            Tracks = new HashSet<Tracks>();
+        }
+
+        public long MediaTypeId { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Tracks> Tracks { get; set; }
+    }
+}
